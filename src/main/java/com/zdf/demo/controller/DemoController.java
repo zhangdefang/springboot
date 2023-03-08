eac509d89d2372541a711fd693d43af5a2413928
zhangdefang <1326471767@qq.com>
2023-03-08 08:56:56 +0800

    后台post接口

--- src/main/java/com/zdf/demo/controller/DemoController.java
+++ src/main/java/com/zdf/demo/controller/DemoController.java
@@ -2,17 +2,24 @@ package com.zdf.demo.controller;
 
 import com.zdf.demo.common.JsonResult;
 import com.zdf.demo.entity.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/dpc")
+@RequestMapping("/dpc/api")
 public class DemoController {
  
  
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public JsonResult test() {
         return JsonResult.ok("查询成功").data(new Person(2.22,3.44,4.55,44));
+    }
+
+    @PostMapping("/logo")
+    public JsonResult logo(@RequestBody User summit){
+        if(!StringUtils.isEmpty(summit.getUsername()) ){
+            return JsonResult.ok("登录成功").data(true);
+        }
+        return JsonResult.ok("登录失败").data(false);
     }
 }
